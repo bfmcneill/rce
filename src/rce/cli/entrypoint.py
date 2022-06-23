b1f0de6872ea5cdc146119d86b007d6d6ef8fa4d
Ben McNeill <16538715+bfmcneill@users.noreply.github.com>
2022-06-23 15:48:02 -0600

    cli entrypoint

--- src/rce/cli/entrypoint.py
+++ src/rce/cli/entrypoint.py
@@ -1,4 +1,14 @@
+import logging
+import time
 import click
+import sqlalchemy as sa
+from rce.database.sqlalchemybase import SqlAlchemyBase
+from rce import settings
+from rce.secret.database_secret import DatabaseSecret
+from rce import service
+from rce import dt
+
+logger = logging.getLogger(__name__)
 
 
 @click.group()
@@ -7,5 +17,30 @@ def cli():
 
 
 @cli.command()
-def drop():
-    click.echo("dropping")
+def create_tables():
+    dbs = DatabaseSecret.from_json_config(settings.config_json_path)
+    engine = sa.create_engine(dbs.uri)
+    SqlAlchemyBase.metadata.create_all(engine)
+
+
+@cli.command()
+@click.option("--subreddit", "-s", required=True)
+@click.option("--after", "-a", default=None)
+@click.option("--before", "-b", default=None)
+@click.option("--tz", default="America/Denver")
+def comments(subreddit, after, before, tz):
+    dr = dt.date_range(after, before, tz)
+    start = time.time()
+    service.comments(
+        subreddit=subreddit,
+        after=int(dr["after"].timestamp()),
+        before=int(dr["before"].timestamp()),
+    )
+    end = time.time()
+    duration = int(end - start)
+    logger.warning(f"duration: {duration} s")
+    click.echo(f"duration: {duration} s")
+
+
+if __name__ == "__main__":
+    cli()
